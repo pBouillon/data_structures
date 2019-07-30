aea6f321633b869c98d0e9136408d40f86fc2f09
pbouillon <pro.pierre.bouillon@protonmail.com>
2019-07-30 22:57:35 +0200

    Completing LinkedList logic

--- lists/linked_list/LinkedList/LinkedList.cs
+++ lists/linked_list/LinkedList/LinkedList.cs
@@ -5,7 +5,11 @@ namespace DataStructures
 {
     public class LinkedList<T>
     {
+        public int Count { get; private set; } = 0;
+
         public LinkedListNode<T> Head { get; private set; }
+
+        public LinkedListNode<T> Tail { get; private set; }
 
         public LinkedList(IEnumerable<T> values = null)
         {
@@ -20,6 +24,9 @@ namespace DataStructures
         public void Add(T value)
         {
             var newNode = new LinkedListNode<T>(value);
+            ++Count;
+
+            Tail = newNode;
 
             if (Head == null)
             {
@@ -38,32 +45,99 @@ namespace DataStructures
 
         public void Append(T value)
         {
-            // TODO
-            throw new NotImplementedException();
+            var newNode = new LinkedListNode<T>(value);
+            ++Count;
+
+            Tail.Next = newNode;
+            Tail = newNode;
         }
 
         public bool Contains(T value)
         {
-            // TODO
-            throw new NotImplementedException();
+            for (var parsedNode = Head; parsedNode != null; parsedNode = parsedNode.Next)
+            {
+                if (parsedNode.Value.Equals(value))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public int Find(T value)
         {
-            // TODO
-            throw new NotImplementedException();
+            var totalParsed = 0;
+
+            for (var parsedNode = Head; parsedNode != null; parsedNode = parsedNode.Next, ++totalParsed)
+            {
+                if (parsedNode.Value.Equals(value))
+                {
+                    return totalParsed;
+                }
+            }
+
+            return -1;
         }
 
         public void Insert(T value, int index)
         {
-            // TODO
-            throw new NotImplementedException();
+            if (index >= Count)
+            {
+                throw new IndexOutOfRangeException(
+                    "Index specified larger than the number of elements in the queue");
+            }
+
+            var totalParsed = 0;
+            LinkedListNode<T> currentNode;
+
+            for (currentNode = Head; totalParsed < index; currentNode = currentNode.Next, ++totalParsed)
+            {
+                if (totalParsed == index - 1)
+                {
+                    break;
+                }
+            }
+
+            var newNode = new LinkedListNode<T>(value, currentNode.Next);
+            currentNode.Next = newNode;
         }
 
         public void Remove(int index)
         {
-            // TODO
-            throw new NotImplementedException();
+            if (index >= Count)
+            {
+                throw new IndexOutOfRangeException(
+                    "Index specified larger than the number of elements in the queue");
+            }
+
+            var totalParsed = 0;
+            LinkedListNode<T> currentNode;
+
+            for (currentNode = Head; totalParsed < index - 1; currentNode = currentNode.Next, ++totalParsed)
+            {
+                if (totalParsed == index - 1)
+                {
+                    break;
+                }
+            }
+
+            currentNode.Next = currentNode.Next.Next;
+        }
+
+        public void RemoveFirst()
+        {
+            Head = Head.Next;
+        }
+
+        public void RemoveLast()
+        {
+            LinkedListNode<T> currentNode;
+
+            for (currentNode = Head; currentNode.Next != Tail; currentNode = currentNode.Next) { }
+
+            Tail = currentNode;
+            currentNode.Next = null;
         }
     }
 }
